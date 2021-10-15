e4ef6df89575e5f1ba9099a779b6fdd6bef23aef
Evgeniya-junior <evgeniyakravcevich@gmail.com>
2021-10-15 14:15:39 +0300

    style in the about block

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,9 +1,14 @@
-function test(a, b) {
-    return a + b
-}
 
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
 
+// $(document).ready(function(){
+//     $('.slider').slick();
+// });
+
+
+//     $('.slider').slick({
+//       setting-name: setting-value
+//     });
+//   });
+
+//   $('.single-item').slick();
 //# sourceMappingURL=script.js.map
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,11 @@
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
+// $(document).ready(function(){
+//     $('.slider').slick();
+// });
+
+
+//     $('.slider').slick({
+//       setting-name: setting-value
+//     });
+//   });
+
+//   $('.single-item').slick();
